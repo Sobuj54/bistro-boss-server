6d9f70114832253448c60b62ee696a621ebeeb49
soubj <sobujahmed124@gmail.com>
2023-12-02 23:02:22 +0600

    api to update user role to admin

--- index.js
+++ index.js
@@ -31,9 +31,35 @@ async function run() {
     const cartCollection = client.db("bistroDB").collection("carts");
 
     // users related api
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
+      const query = { email: user.email };
+      const userExists = await userCollection.findOne(query);
+
+      if (userExists) {
+        return res.send({ message: "user already exists" });
+      }
+
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
