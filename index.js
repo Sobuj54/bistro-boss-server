1b35c2af55a48c4c8f100712a4e73ce4714ca928
soubj <sobujahmed124@gmail.com>
2023-12-03 22:48:56 +0600

    check for admin

--- index.js
+++ index.js
@@ -78,6 +78,19 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
